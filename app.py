36bc98fcd29e44a0215adb23edc2ca0b20658ae9
GabrielCarvalho2209 <rm93857@estudante.fieb.edu.br>
2025-09-03 11:24:57 -0300

    atulização

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def IndexLanding():
     return render_template('IndexLanding.html')
-', methods=['GET', 'POST'])
 
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('login-user')  
-        password = request.form.get('login-pass') 
+        username = request.form.get('login-user')
+        password = request.form.get('login-pass')
         print(f"Usuário: {username}, Senha: {password}")
         return "Login processado!"
     return render_template('Login.html')
+
+    @app.route ('/cadastro', methos=['GET', 'POST'])
+    def cadastro():
+        return render_template ('Cadastro.html')
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
